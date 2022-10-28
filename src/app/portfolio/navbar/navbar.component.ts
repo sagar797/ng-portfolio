9c50dc1db9ab962e7a28fe9e79da9db2d4da9dbb
sagar gupta <gupta.sagar797@gmail.com>
2022-10-29 02:28:57 +0530

    added resume link function

--- src/app/portfolio/navbar/navbar.component.ts
+++ src/app/portfolio/navbar/navbar.component.ts
@@ -14,4 +14,10 @@ export class NavbarComponent implements OnInit {
   redirectToHomePage() {
     this.router.navigate(['']);
   }
+
+  redirectToResume() {
+    window.open(
+      'https://docs.google.com/document/d/15fXS2xZu0JwI5XEeWEq8a-P94FQWz0QmAjjlwZi7o1Y/edit'
+    );
+  }
 }
