e40081b5fa337617f7d3b6b432f793854c6e8b39
sagar gupta <gupta.sagar797@gmail.com>
2022-10-28 23:18:56 +0530

    projects canvas setup

--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { EducationComponent } from './education/education.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { HomeComponent } from './home/home.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -37,10 +36,6 @@ const routes: Routes = [
       {
         path: 'projects',
         component: ProjectsComponent,
-      },
-      {
-        path: 'education',
-        component: EducationComponent,
       },
     ],
   },
--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -5,7 +5,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { EducationComponent } from './education/education.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutComponent } from './layout/layout.component';
 import { NgParticlesModule } from 'ng-particles';
@@ -19,7 +18,6 @@ import { SkillsComponent } from './skills/skills.component';
     AboutComponent,
     ExperienceComponent,
     ProjectsComponent,
-    EducationComponent,
     LayoutComponent,
     SkillsComponent,
   ],
