90f1f6c4933dc3c329b4e9750504de545324b20f
sagar gupta <gupta.sagar797@gmail.com>
2022-10-29 15:39:19 +0530

    added skills with icons (mdb + images)

--- src/app/portfolio/skills/skills.component.ts
+++ src/app/portfolio/skills/skills.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -17,54 +16,85 @@ export class SkillsComponent implements OnInit {
   getSkills() {
     this.skills = [
       {
+        name: 'Data Structure & Algorithms',
+        progress: 75,
+        class: 'fas fa-code',
+        image: false,
+      },
+      {
+        name: 'Angular',
+        progress: 50,
+        class: 'fab fa-angular',
+        image: false,
+      },
+      {
         name: 'HTML',
         progress: 75,
         class: 'fa fa-html5',
+        image: false,
       },
       {
         name: 'CSS',
         progress: 50,
         class: 'fa fa-css3',
+        image: false,
       },
       {
         name: 'Javascript',
         progress: 75,
-        class: 'fa-brands fa-angular',
+        class: 'fab fa-js',
+        image: false,
       },
       {
-        name: 'CSS',
-        progress: 50,
-        class: 'fa fa-html5',
-      },
-      {
-        name: 'HTML',
+        name: 'Python',
         progress: 75,
-        class: 'fa fa-html5',
+        class: 'fab fa-python',
+        image: false,
       },
       {
-        name: 'CSS',
+        name: 'Flask',
         progress: 50,
-        class: 'fa fa-html5',
+        class: 'fas fa-pepper-hot',
+        image: false,
       },
       {
-        name: 'HTML',
+        name: 'AWS',
         progress: 75,
-        class: 'fa fa-html5',
+        class: 'aws',
+        path: '../../../assets/images/skills/aws.png',
+        image: true,
       },
       {
-        name: 'CSS',
+        name: 'Dynamo DB',
         progress: 50,
-        class: 'fa fa-html5',
+        class: 'dynamo',
+        path: '../../../assets/images/skills/dynamo.png',
+        image: true,
       },
       {
-        name: 'HTML',
-        progress: 75,
-        class: 'fa fa-html5',
-      },
-      {
-        name: 'CSS',
+        name: 'AWS Lambda',
         progress: 50,
-        class: 'fa fa-html5',
+        class: 'lambda',
+        path: '../../../assets/images/skills/lambda.png',
+        image: true,
+      },
+      {
+        name: 'Dynamo DB',
+        progress: 50,
+        class: 'fas fa-database',
+        image: false,
+      },
+      {
+        name: 'Dynamo DB',
+        progress: 50,
+        class: 'fas fa-database',
+        image: false,
+      },
+      {
+        name: 'Dynamo DB',
+        progress: 50,
+        class: 'fas fa-database',
+        image: false,
       },
     ];
   }
