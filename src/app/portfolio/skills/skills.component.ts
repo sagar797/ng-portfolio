1353e37454725eac8a50f671ac71fe451a600841
sagar gupta <gupta.sagar797@gmail.com>
2022-10-29 13:12:34 +0530

    added skills section

--- src/app/portfolio/skills/skills.component.ts
+++ src/app/portfolio/skills/skills.component.ts
@@ -3,13 +3,69 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
+  skills = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.getSkills();
   }
 
+  getSkills() {
+    this.skills = [
+      {
+        name: 'HTML',
+        progress: 75,
+        class: 'fa fa-html5',
+      },
+      {
+        name: 'CSS',
+        progress: 50,
+        class: 'fa fa-css3',
+      },
+      {
+        name: 'Javascript',
+        progress: 75,
+        class: 'fa-brands fa-angular',
+      },
+      {
+        name: 'CSS',
+        progress: 50,
+        class: 'fa fa-html5',
+      },
+      {
+        name: 'HTML',
+        progress: 75,
+        class: 'fa fa-html5',
+      },
+      {
+        name: 'CSS',
+        progress: 50,
+        class: 'fa fa-html5',
+      },
+      {
+        name: 'HTML',
+        progress: 75,
+        class: 'fa fa-html5',
+      },
+      {
+        name: 'CSS',
+        progress: 50,
+        class: 'fa fa-html5',
+      },
+      {
+        name: 'HTML',
+        progress: 75,
+        class: 'fa fa-html5',
+      },
+      {
+        name: 'CSS',
+        progress: 50,
+        class: 'fa fa-html5',
+      },
+    ];
+  }
 }
