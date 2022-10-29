71f2fa2a2a034322406d3228ba211c6d0be5348a
sagar gupta <gupta.sagar797@gmail.com>
2022-10-30 00:45:11 +0530

    added portfolio link and used calc() in css

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -2,6 +2,6 @@ export const SOCIAL_HANDLE_LINKS: any = {
   linkedin: 'https://www.linkedin.com/in/sagar797/',
   leetcode: 'https://leetcode.com/sagar797/',
   github: 'https://github.com/sagar797',
-  portfolio: 'localhost:4200', // todo - to update once deployed
+  portfolio: 'https://sagarportfolio.herokuapp.com/',
   twitter: 'https://twitter.com/sagar797',
 };
