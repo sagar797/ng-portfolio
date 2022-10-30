9a103d5d5c83188217e98a59e4e628b14981ddbe
sagar gupta <gupta.sagar797@gmail.com>
2022-10-30 12:50:17 +0530

    projects added dynamically with array of objects

--- src/app/portfolio/projects/projects.component.ts
+++ src/app/portfolio/projects/projects.component.ts
@@ -6,7 +6,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  projects = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProjectList();
+  }
+
+  getProjectList() {
+    this.projects = [
+      {
+        name: 'Master ToDo List',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consectetur? Blanditiis quos voluptatem quas sequi deleniti, amet, nam voluptatibus, impedit alias soluta similique atque maiores sunt delectus a.',
+        tech_used: ['Vs Code', 'Sublime Text', 'Atom', 'iTerm2', 'Hyper'],
+        image: '../../../assets/images/projects/todo-laptop.png',
+        githubLink: 'https://github.com/sagar797/flask-todo',
+        hostedLink: 'http://sagar-todo-app.herokuapp.com/',
+      },
+      {
+        name: 'Master ToDo List',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consectetur? Blanditiis quos voluptatem quas sequi deleniti, amet, nam voluptatibus, impedit alias soluta similique atque maiores sunt delectus a.',
+        tech_used: ['Vs Code', 'Sublime Text', 'Atom', 'iTerm2', 'Hyper'],
+        image: '../../../assets/images/projects/todo-laptop.png',
+        githubLink: 'https://github.com/sagar797/flask-todo',
+        hostedLink: 'http://sagar-todo-app.herokuapp.com/',
+      },
+      {
+        name: 'Master ToDo List',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consectetur? Blanditiis quos voluptatem quas sequi deleniti, amet, nam voluptatibus, impedit alias soluta similique atque maiores sunt delectus a.',
+        tech_used: ['Vs Code', 'Sublime Text', 'Atom', 'iTerm2', 'Hyper'],
+        image: '../../../assets/images/projects/todo-laptop.png',
+        githubLink: 'https://github.com/sagar797/flask-todo',
+        hostedLink: 'http://sagar-todo-app.herokuapp.com/',
+      },
+      {
+        name: 'Master ToDo List',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consectetur? Blanditiis quos voluptatem quas sequi deleniti, amet, nam voluptatibus, impedit alias soluta similique atque maiores sunt delectus a.',
+        tech_used: ['Vs Code', 'Sublime Text', 'Atom', 'iTerm2', 'Hyper'],
+        image: '../../../assets/images/projects/todo-laptop.png',
+        githubLink: 'https://github.com/sagar797/flask-todo',
+        hostedLink: 'http://sagar-todo-app.herokuapp.com/',
+      },
+      {
+        name: 'Master ToDo List',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consectetur? Blanditiis quos voluptatem quas sequi deleniti, amet, nam voluptatibus, impedit alias soluta similique atque maiores sunt delectus a.',
+        tech_used: ['Vs Code', 'Sublime Text', 'Atom', 'iTerm2', 'Hyper'],
+        image: '../../../assets/images/projects/todo-laptop.png',
+        githubLink: 'https://github.com/sagar797/flask-todo',
+        hostedLink: 'http://sagar-todo-app.herokuapp.com/',
+      },
+      {
+        name: 'Master ToDo List',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consectetur? Blanditiis quos voluptatem quas sequi deleniti, amet, nam voluptatibus, impedit alias soluta similique atque maiores sunt delectus a.',
+        tech_used: ['Vs Code', 'Sublime Text', 'Atom', 'iTerm2', 'Hyper'],
+        image: '../../../assets/images/projects/todo-laptop.png',
+        githubLink: 'https://github.com/sagar797/flask-todo',
+        hostedLink: 'http://sagar-todo-app.herokuapp.com/',
+      },
+    ];
+  }
+
+  redirectToLink(url) {
+    window.open(url);
+  }
 }
