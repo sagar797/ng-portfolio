b3241a835e24183c3f7f8f348dc3f1757771f37c
sagar gupta <gupta.sagar797@gmail.com>
2022-11-02 13:23:02 +0530

    converted experience data from static to dynamic

--- src/app/portfolio/experience/experience.component.ts
+++ src/app/portfolio/experience/experience.component.ts
@@ -6,7 +6,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent implements OnInit {
+  experiences = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllExperiences();
+  }
+
+  getAllExperiences() {
+    this.experiences = [
+      {
+        role: 'Full-Stack Developer',
+        company: {
+          name: 'Delhivery',
+          logo: '../../../assets/images/delhivery.png',
+        },
+        timeline: {
+          from: 'April 2021',
+          to: 'Present',
+        },
+        information: [
+          'Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.',
+          'Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.',
+          'Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.',
+        ],
+      },
+      {
+        role: 'Full-Stack Developer',
+        company: {
+          name: 'Delhivery',
+          logo: '../../../assets/images/delhivery.png',
+        },
+        timeline: {
+          from: 'April 2021',
+          to: 'Present',
+        },
+        information: [
+          'Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.',
+          'Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.',
+          'Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.',
+        ],
+      },
+      {
+        role: 'Full-Stack Developer',
+        company: {
+          name: 'Delhivery',
+          logo: '../../../assets/images/delhivery.png',
+        },
+        timeline: {
+          from: 'April 2021',
+          to: 'Present',
+        },
+        information: [
+          'Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.',
+          'Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.',
+          'Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.',
+        ],
+      },
+      {
+        role: 'Full-Stack Developer',
+        company: {
+          name: 'Delhivery',
+          logo: '../../../assets/images/delhivery.png',
+        },
+        timeline: {
+          from: 'April 2021',
+          to: 'Present',
+        },
+        information: [
+          'Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.',
+          'Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.',
+          'Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.',
+        ],
+      },
+    ];
+  }
 }
