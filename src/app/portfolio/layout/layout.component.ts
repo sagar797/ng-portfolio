3d19d2f343d52cd10a3a83a4317ca04a3f543e70
sagar gupta <gupta.sagar797@gmail.com>
2022-11-02 15:21:25 +0530

    layout loader for each route

--- src/app/portfolio/layout/layout.component.ts
+++ src/app/portfolio/layout/layout.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  isLoading: boolean = false;
+  loadingTime: number = 0;
+  loaderTime: number = 3000;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.loadingTime = this.loaderTime;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, this.loaderTime);
+    setInterval(() => {
+      this.loadingTime -= 1000;
+    }, 1000);
   }
-
 }
